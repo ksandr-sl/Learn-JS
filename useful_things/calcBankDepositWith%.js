86301af5f81ee54f70c99c7f1a69b0c2cff76e91
ksandrsl <ksandr.sl@gmail.com>
2022-06-27 20:27:05 +0300

    some refactoring

--- useful_things/calcBankDepositWith%.js
+++ useful_things/calcBankDepositWith%.js
@@ -1,9 +1,9 @@
-calcYearlyDepositIncome = (depositSize, percent) => {
+function calcYearlyDepositIncome(depositSize, percent) {
     let months = 12;
     let totalIncome = depositSize;
     while (months != 0) {
         totalIncome += Math.floor(totalIncome / 100 * percent / 12);
         months--;
     }
-    return totalIncome - depositSize;
+    return +(totalIncome - depositSize).toFixed(2);
 } //вычисляет доход от годового вклада с капитализацией процентов
